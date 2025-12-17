a800ab62d45fccc2437a8492764e1a753bd62e20
Michela <micheladellagatta1@gmail.com>
2025-12-17 16:53:14 +0100

    Add jumbotron to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
   return (
     <>
-      <div className="container">
-        <h1 className="font-bold mb-4">BooRoad</h1>
-        <p>BooRoad details</p>
+      <div
+        className="p-5 text-center bg-image rounded-3 position-relative"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1519885277449-12eee5564d68?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+          height: "400px",
+          backgroundSize: "cover",
+          backgroundPosition: "center bottom",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div
+          className="position-absolute top-0 start-0 w-100 h-100"
+          style={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+        >
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+              <h1 className="mb-3">BooRoad</h1>
+              <h4 className="mb-3">
+                Destinazioni, date e dettagli delle tue prossime avventure
+              </h4>
+              <Link to={`/listaviaggi`}>
+                <button className="btn btn-outline-light">Lista viaggi</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
