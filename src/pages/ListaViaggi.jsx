12cfe25ab6fc6552a359e70dfdcc1c0a35e3a39c
Michela <micheladellagatta1@gmail.com>
2025-12-17 15:28:46 +0100

    Refactor ListaViaggi component to display trips in a card layout

--- src/pages/ListaViaggi.jsx
+++ src/pages/ListaViaggi.jsx
@@ -4,16 +4,27 @@ export default function ListaViaggi() {
   return (
     <>
       <div className="container">
-        <h1>Lista Viaggi</h1>
-        <ul>
+        <h1 className="my-3">Lista Viaggi</h1>
+        <div className="row">
           {viaggi.map((viaggio) => (
-            <li key={viaggio.id}>
-              <strong>{viaggio.destinazione}</strong>
-              <br />
-              {viaggio.dataInizio} &ndash; {viaggio.dataFine}
-            </li>
+            <div key={viaggio.id} className="col-md-6 col-lg-3 mb-4">
+              <div className="card h-100">
+                <div className="card-body">
+                  <h5 className="card-title">{viaggio.destinazione}</h5>
+
+                  <p className="card-text">
+                    Dal: <strong>{viaggio.dataInizio}</strong> <br />
+                    al <strong>{viaggio.dataFine}</strong>
+                  </p>
+
+                  <a href="#" className="btn btn-dark">
+                    Dettagli viaggio
+                  </a>
+                </div>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </>
   );
