b8590623d954d1d5e68a2640793ef936bd95917c
Michela <micheladellagatta1@gmail.com>
2025-12-17 15:17:09 +0100

    Display trips with related details

--- src/pages/ListaViaggi.jsx
+++ src/pages/ListaViaggi.jsx
@@ -1,8 +1,19 @@
+import viaggi from "../data/viaggi.js";
+
 export default function ListaViaggi() {
   return (
     <>
       <div className="container">
         <h1>Lista Viaggi</h1>
+        <ul>
+          {viaggi.map((viaggio) => (
+            <li key={viaggio.id}>
+              <strong>{viaggio.destinazione}</strong>
+              <br />
+              {viaggio.dataInizio} &ndash; {viaggio.dataFine}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
